3b07f26387a2fb59d1f03f28aecb79d9967e52ec
CyberL1 <mateusz.hf18@gmail.com>
2024-02-28 17:05:28 +0100

    Generate as Image by default

--- main.ts
+++ main.ts
@@ -1,13 +1,20 @@
-const trash = (req: Request) => {
+import { generateImage } from "./generate.ts";
+
+const trash = async (req: Request) => {
   const url = new URL(req.url);
   const thing = url.searchParams.get("thing");
 
   let trash = Deno.readTextFileSync("./trash.txt");
 
-  if (!thing) trash = trash.replace(" <- this is", "").replace("trash", "");
-  else trash = trash.replace("trash", thing);
+  if (url.pathname === "/text") {
+    if (!thing) trash = trash.replace(" <- this is", "").replace("trash", "");
+    else trash = trash.replace("trash", thing);
 
-  return new Response(trash);
+    return new Response(trash);
+  }
+
+  const image = await generateImage(thing);
+  return new Response(image);
 };
 
 Deno.serve(trash);
