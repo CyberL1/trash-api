cb6fad28db5e7ef4f6c264a31326d459df109dcc
CyberL1 <cyber.hf18@gmail.com>
2024-02-28 19:44:54 +0100

    Respond with image no matter what

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 import { generateImage } from "./generate.ts";
-import { encode } from "https://deno.land/std@0.166.0/encoding/base64.ts";
 
 const trash = async (req: Request) => {
   const url = new URL(req.url);
@@ -16,19 +15,7 @@ const trash = async (req: Request) => {
 
   const image = await generateImage(thing);
 
-  const html = `<html>
-  <body style="margin: 0">
-    <img style="width: 100%;height: 100%;" src="data:image/png;base64,${
-    encode(image)
-  }" />
-  </body>
-</html>`;
-
-  if (req.headers.get("user-agent")?.includes("Discord")) {
-    return new Response(image, { headers: { "content-type": "image/png" } });
-  }
-
-  return new Response(html, { headers: { "content-type": "text/html" } });
+  return new Response(image, { headers: { "content-type": "image/png" } });
 };
 
 Deno.serve(trash);
